36ec60b9582ba6b9df7da586e158f6960cbee44c
nicorfu <nf1195@nyu.edu>
2024-06-27 21:25:38 -0400

    added load_texture() (previous commit should've clarified that i set up initial speeds and increments (rot, scale), as well as variables to keep time)

--- project 2/main.cpp
+++ project 2/main.cpp
@@ -125,6 +125,36 @@ void render();
 void shutdown();
 
 
+GLuint load_texture(const char* filepath)
+{
+	int width;
+	int height;
+	int num_components;
+
+	unsigned char* image = stbi_load(filepath, &width, &height, &num_components, STBI_rgb_alpha);
+
+	if (image == NULL)
+	{
+		LOG("Can't load image. Smh. Check filepath.");
+		assert(false);
+	}
+
+	GLuint textureID;
+	glGenTextures(NUMBER_OF_TEXTURES, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER,
+		GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	stbi_image_free(image);
+
+	return textureID;
+}
+
+
 void initialize()
 {
 	SDL_Init(SDL_INIT_VIDEO);
