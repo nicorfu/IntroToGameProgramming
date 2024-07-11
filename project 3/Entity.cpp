18e7194e4685038b2559ae4708bb471c82beda93
nicorfu <nf1195@nyu.edu>
2024-07-11 18:08:48 -0400

    began to populate Entity.h & pasted #include's and #define's into Entity.cpp

---
+++ project 3/Entity.cpp
@@ -0,0 +1,12 @@
+#include <GL/glew.h>
+
+#define GL_SILENCE_DEPRECATION
+#define STB_IMAGE_IMPLEMENTATION
+#define GL_GLEXT_PROTOTYPES 1
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+#include "Entity.h"
--- project 3/Entity.h
+++ project 3/Entity.h
@@ -1,1 +1,7 @@
-#pragma once
+class Entity
+{
+private:
+	bool m_is_active = true;
+
+	glm::vec3 m_position;
+};
