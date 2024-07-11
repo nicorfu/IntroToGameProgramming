b6b116db8ae4b3de0d7bf66acbe69ad841be8729
nicorfu <nf1195@nyu.edu>
2024-07-11 01:20:10 -0400

    added intro comment block

--- project 3/main.cpp
+++ project 3/main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Nico Flores
+* Assignment: Lunar Lander
+* Date due: 2024-07-13, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include <GL/glew.h>
 
 #define GL_SILENCE_DEPRECATION
