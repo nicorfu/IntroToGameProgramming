bb8c9fa3b9b01c9f095967ee63508731e2fc84ce
nicorfu <nf1195@nyu.edu>
2024-07-13 15:03:19 -0400

    ship finally stops correctly when colliding with bottom of platforms

--- project 3/Entity.cpp
+++ project 3/Entity.cpp
@@ -93,7 +93,7 @@ void const Entity::check_collision_y(Entity* collidable_entities, int collidable
 
 		if (check_collision(collidable_entity))
 		{
-			float y_distance = fabs(m_position.y - collidable_entity->m_position.y);
+			float y_distance = fabs(m_position.y - (collidable_entity->m_position.y - 0.25f));
 			float y_overlap = fabs(y_distance - (m_height / 2.0f) - (collidable_entity->m_height / 2.0f));
 
 			if (m_velocity.y > 0)
@@ -174,7 +174,7 @@ bool const Entity::check_collision(Entity* other) const
 	}
 
 	float x_distance = fabs(m_position.x - other->m_position.x) - ((m_width + other->m_width) / 2.0f);
-	float y_distance = fabs(m_position.y - other->m_position.y) - ((m_height + other->m_height) / 2.0f);
+	float y_distance = fabs(m_position.y - (other->m_position.y - 0.25f)) - ((m_height + other->m_height) / 2.0f);
 
 	return x_distance < 0.0f && y_distance < 0.0f;
 }
