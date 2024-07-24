39511d84d1637a1547d8e74a54cd9df765995782
nicorfu <nf1195@nyu.edu>
2024-07-23 23:21:17 -0400

    added build() to Map.cpp

--- project 4/Map.cpp
+++ project 4/Map.cpp
@@ -16,7 +16,53 @@ Map::Map(int width, int height, unsigned int* level_data, GLuint texture_id, flo
 	build();
 }
 
-void Map::build();
+void Map::build()
+{
+	for (int y_coord = 0; y_coord < m_height; y_coord++)
+	{
+		for (int x_coord = 0; x_coord < m_width; x_coord++)
+		{
+			int tile = m_level_data[y_coord * m_width + x_coord];
+
+			if (tile == 0)
+			{
+				continue;
+			}
+
+			float u_coord = (float)(tile % m_tile_count_x) / (float)m_tile_count_x;
+			float v_coord = (float)(tile / m_tile_count_x) / (float)m_tile_count_y;
+
+			float tile_width = 1.0f / (float)m_tile_count_x;
+			float tile_height = 1.0f / (float)m_tile_count_y;
+
+			float x_offset = -(m_tile_size / 2);
+			float y_offset = (m_tile_size / 2);
+
+			m_vertices.insert(m_vertices.end(), {
+				x_offset + (m_tile_size * x_coord),  y_offset + -m_tile_size * y_coord,
+				x_offset + (m_tile_size * x_coord),  y_offset + (-m_tile_size * y_coord) - m_tile_size,
+				x_offset + (m_tile_size * x_coord) + m_tile_size, y_offset + (-m_tile_size * y_coord) - m_tile_size,
+				x_offset + (m_tile_size * x_coord), y_offset + -m_tile_size * y_coord,
+				x_offset + (m_tile_size * x_coord) + m_tile_size, y_offset + (-m_tile_size * y_coord) - m_tile_size,
+				x_offset + (m_tile_size * x_coord) + m_tile_size, y_offset + -m_tile_size * y_coord
+				});
+
+			m_texture_coordinates.insert(m_texture_coordinates.end(), {
+				u_coord, v_coord,
+				u_coord, v_coord + (tile_height),
+				u_coord + tile_width, v_coord + (tile_height),
+				u_coord, v_coord,
+				u_coord + tile_width, v_coord + (tile_height),
+				u_coord + tile_width, v_coord
+				});
+		}
+	}
+
+	m_left_bound = 0 - (m_tile_size / 2);
+	m_right_bound = (m_tile_size * m_width) - (m_tile_size / 2);
+	m_top_bound = 0 + (m_tile_size / 2);
+	m_bottom_bound = -(m_tile_size * m_height) + (m_tile_size / 2);
+}
 
 void Map::render(ShaderProgram* program);
 
