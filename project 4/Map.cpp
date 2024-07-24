f7843cc419a36661c92233f0d3e432283ef7a455
nicorfu <nf1195@nyu.edu>
2024-07-23 23:40:55 -0400

    added is_solid() to Map.cpp

--- project 4/Map.cpp
+++ project 4/Map.cpp
@@ -1,5 +1,6 @@
 #include "Map.h"
 
+
 Map::Map(int width, int height, unsigned int* level_data, GLuint texture_id, float tile_size, int tile_count_x,
 	int tile_count_y)
 {
@@ -85,5 +86,41 @@ void Map::render(ShaderProgram* program)
 
 bool Map::is_solid(glm::vec3 position, float* penetration_x, float* penetration_y)
 {
+	*penetration_x = 0;
+	*penetration_y = 0;
 
+	if (position.x < m_left_bound || position.x > m_right_bound)
+	{
+		return false;
+	}
+	if (position.y > m_top_bound || position.y < m_bottom_bound)
+	{
+		return false;
+	}
+
+	int tile_x = floor((position.x + (m_tile_size / 2)) / m_tile_size);
+	int tile_y = -(ceil(position.y - (m_tile_size / 2))) / m_tile_size;
+
+	if (tile_x < 0 || tile_x >= m_width)
+	{
+		return false;
+	}
+	if (tile_y < 0 || tile_y >= m_height)
+	{
+		return false;
+	}
+
+	int tile = m_level_data[tile_y * m_width + tile_x];
+	if (tile == 0)
+	{
+		return false;
+	}
+
+	float tile_center_x = tile_x * m_tile_size;
+	float tile_center_y = -(tile_y * m_tile_size);
+
+	*penetration_x = (m_tile_size / 2) - fabs(position.x - tile_center_x);
+	*penetration_y = (m_tile_size / 2) - fabs(position.y - tile_center_y);
+
+	return false;
 }
