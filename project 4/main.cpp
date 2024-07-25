e749dd11fdc59f3a68b791544c61c6c91c7985c5
nicorfu <nf1195@nyu.edu>
2024-07-25 19:21:46 -0400

    main.cpp updated with more basics

--- project 4/main.cpp
+++ project 4/main.cpp
@@ -33,7 +33,8 @@
 #define FIXED_TIMESTEP 0.0166666f
 #define LEVEL1_WIDTH 14
 #define LEVEL1_HEIGHT 5
-#define ENEMY_COUNT 3
+#define ENEMY_COUNT 1
+#define GRAVITY -9.81
 
 
 struct GameState
@@ -41,7 +42,7 @@ struct GameState
 	Entity* player;
 	Entity* enemies;
 
-	Entity* map;
+	Map* map;
 };
 
 unsigned int LEVEL_1_DATA[] =
@@ -54,7 +55,7 @@ GameState g_state;
 bool g_game_is_running = true;
 
 bool GAME_ONGOING = true;
-bool MISSION_FAILED = false;
+bool LOST = false;
 
 SDL_Window* g_display_window;
 ShaderProgram g_shader_program;
@@ -118,7 +119,7 @@ GLuint load_texture(const char* filepath)
 	glBindTexture(GL_TEXTURE_2D, textureID);
 
 	glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER,
-		GL_RGBA, GL_UNSIGNED_BYTE, image);
+				 GL_RGBA, GL_UNSIGNED_BYTE, image);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -137,21 +138,34 @@ void initialize()
 	SDL_Init(SDL_INIT_VIDEO);
 
 	g_display_window = SDL_CreateWindow("Rise of the AI", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+										WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
 
 	if (g_display_window == nullptr)
 	{
-		std::cerr << "Error: SDL window could not be created.\n";
+		LOG("Error: SDL window could not be created.\n");
 		shutdown();
 		exit(1);
 	}
 
 	SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
 	SDL_GL_MakeCurrent(g_display_window, context);
+	if (context == nullptr)
+	{
+		LOG("ERROR: Could not create OpenGL context.\n");
+		shutdown();
+	}
 
 	glewInit();
 
 	glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+	g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
+
+	g_view_matrix = glm::mat4(1.0f);
+	g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+
+	g_shader_program.set_projection_matrix(g_projection_matrix);
+	g_shader_program.set_view_matrix(g_view_matrix);
 
 	glUseProgram(g_shader_program.get_program_id());
 
@@ -161,11 +175,15 @@ void initialize()
 	 
 	//g_state.map = new Map(LEVEL1_WIDTH, LEVEL1_HEIGHT, LEVEL_1_DATA, map_texture_id, 1.0f, 4, 1);
 
+	glm::vec3 acceleration = glm::vec3(0.0f, GRAVITY, 0.0f);
+
 	//GLuint player_texture_id = load_texture(PLAYER_FILEPATH);
 
 	//g_state.player = new Entity();
 
-	//g_state.player->set_entity_type(PLAYER);
+	//int player_walking_animation
+
+	g_state.player->set_jump_power(5.0f);
 
 	//GLuint enemy_texture_id = load_texture(ENEMY_FILEPATH);
 
@@ -174,6 +192,7 @@ void initialize()
 	for (int i = 0; i < ENEMY_COUNT; i++)
 	{
 		//g_state.enemies[i] = Entity(enemy_texture_id, 1.0f, 1.0f, 1.0f, ENEMY, GUARD, IDLE);
+		g_state.enemies[i].set_jump_power(3.0f);
 	}
 
 	g_state.enemies[0].set_position(glm::vec3(3.0f, 0.0f, 0.0f));
@@ -207,9 +226,19 @@ void process_input()
 						g_game_is_running = false;
 						break;
 
+					case SDLK_SPACE:
+						if (g_state.player->get_collided_bottom())
+						{
+							g_state.player->jump();
+						}
+						break;
+
 					default:
 						break;
 				}
+
+			default:
+				break;
 		}
 	}
 
@@ -230,7 +259,7 @@ void process_input()
 
 		if (glm::length(g_state.player->m_movement) > 1.0f)
 		{
-			g_state.player->m_movement = glm::normalize(g_state.player->m_movement);
+			g_state.player->normalize_movement();
 		}
 	}
 	*/
@@ -248,23 +277,33 @@ void update()
 	if (delta_time < FIXED_TIMESTEP)
 	{
 		g_accumulator = delta_time;
+
 		return;
 	}
 
 	while (delta_time >= FIXED_TIMESTEP)
 	{
 		g_state.player->update(FIXED_TIMESTEP, g_state.player, NULL, 0, g_state.map);
+
 		for (int i = 0; i < ENEMY_COUNT; i++)
 		{
 			//g_state.enemies[i].update(FIXED_TIMESTEP, g_state.player, g_state.platforms, PLATFORM_COUNT);
 		}
+
 		delta_time -= FIXED_TIMESTEP;
 	}
+
+	g_accumulator = delta_time;
+
+	g_view_matrix = glm::mat4(1.0f);
+	g_view_matrix = glm::translate(g_view_matrix, glm::vec3(-g_state.player->get_position().x, 0.0f, 0.0f));
 }
 
 
 void render()
 {
+	g_shader_program.set_view_matrix(g_view_matrix);
+
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	g_state.map->render(&g_shader_program);
@@ -285,7 +324,7 @@ void shutdown()
 
 	delete g_state.map;
 	delete g_state.player;
-	delete g_state.enemies;
+	delete [] g_state.enemies;
 }
 
 
