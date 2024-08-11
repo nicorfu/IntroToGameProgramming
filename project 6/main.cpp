329a2687e1bc4bc6ae5e1d1a04db8ed8d672ad46
nicorfu <nf1195@nyu.edu>
2024-08-10 20:27:37 -0400

    created project 6

---
+++ project 6/main.cpp
@@ -0,0 +1,9 @@
+/**
+* Author: Nico Flores
+* Assignment: [Your game's name here]
+* Date due: 2024-08-15, 1:00pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
